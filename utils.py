a9b49f0788be1e989d90a16692898943acbafe33
akhosrvith1998 <a.khosrvith1898@gmail.com>
2025-05-25 14:48:12 +0330

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,78 @@
+import requests
+from datetime import datetime, timezone, timedelta
+import json
+from functools import lru_cache
+
+TOKEN = "7889701836:AAECLBRjjDadhpgJreOctpo5Jc72ekDKNjc"
+URL = f"https://api.telegram.org/bot{TOKEN}/"
+IRST_OFFSET = timedelta(hours=3, minutes=30)
+
+@lru_cache(maxsize=1000)
+def get_user_profile_photo(user_id):
+    """دریافت عکس پروفایل کاربر با کش"""
+    url = URL + "getUserProfilePhotos"
+    params = {"user_id": user_id, "limit": 1}
+    resp = requests.get(url, params=params).json()
+    if resp.get("ok") and resp["result"]["total_count"] > 0:
+        photo = resp["result"]["photos"][0][0]["file_id"]
+        return photo
+    return None
+
+def escape_markdown(text):
+    """فرمت کردن متن برای MarkdownV2"""
+    escape_chars = '_*[]()~>#+-=|{}.!'
+    return ''.join(['\\' + char if char in escape_chars else char for char in text])
+
+def get_irst_time(timestamp):
+    """تبدیل زمان به وقت ایران"""
+    utc_time = datetime.fromtimestamp(timestamp, tz=timezone.utc)
+    irst_time = utc_time + IRST_OFFSET
+    return irst_time.strftime("%H:%M")
+
+def answer_inline_query(inline_query_id, results):
+    """پاسخ به inline query"""
+    url = URL + "answerInlineQuery"
+    data = {
+        "inline_query_id": inline_query_id,
+        "results": json.dumps(results),
+        "cache_time": 0,
+        "is_personal": True
+    }
+    requests.post(url, data=data)
+
+def answer_callback_query(callback_query_id, text, show_alert=False):
+    """پاسخ به callback query"""
+    url = URL + "answerCallbackQuery"
+    data = {
+        "callback_query_id": callback_query_id,
+        "text": text,
+        "show_alert": show_alert
+    }
+    requests.post(url, data=data)
+
+def edit_message_text(chat_id=None, message_id=None, inline_message_id=None, text=None, reply_markup=None):
+    """ویرایش پیام"""
+    url = URL + "editMessageText"
+    data = {
+        "text": text,
+        "parse_mode": "MarkdownV2",
+        "reply_markup": json.dumps(reply_markup) if reply_markup else None
+    }
+    if chat_id and message_id:
+        data["chat_id"] = chat_id
+        data["message_id"] = message_id
+    elif inline_message_id:
+        data["inline_message_id"] = inline_message_id
+    else:
+        raise ValueError("Either (chat_id and message_id) or inline_message_id must be provided.")
+    return requests.post(url, data=data)
+
+def format_block_code(whisper_data):
+    """فرمت کردن اطلاعات نجوا"""
+    receiver_display_name = whisper_data['receiver_display_name']
+    view_times = whisper_data.get("receiver_views", [])
+    view_count = len(view_times)
+    view_time_str = get_irst_time(view_times[-1]) if view_times else "هنوز دیده نشده"
+    code_content = f"{escape_markdown(receiver_display_name)} {view_count} | {view_time_str}\n___"
+    code_content += "\n" + ("\n".join([escape_markdown(user) for user in whisper_data["curious_users"]]) if whisper_data["curious_users"] else "Nothing")
+    return code_content
